0db68d401893458455b33d2282e3d0e9d1c45ecf
arthur190901 <khachatryan184@icloud.com>
2022-03-08 05:13:18 +0300

    add.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.main-header__button');
+const show = document.querySelector('.main-header__list');
+
+burger.addEventListener('click', function(evt){
+  evt.preventDefault;
+})
+
