80119de7788d8aa5e334dc91c86455c79761f10e
arthur190901 <khachatryan184@icloud.com>
2022-03-13 21:53:50 +0300

    a new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 {
-const btn = document.getElementById('button');
-const burgerMenu = document.querySelector('.sections');
-const close = document.querySelector('.close');
+  const btn = document.getElementById('button');
+  const burgerMenu = document.querySelector('.sections');
+  const closeMenu = document.querySelector('.close');
 
-btn.addEventListener('click', function(evt){
-  evt.preventDefault;
-  burgerMenu.classList.toggle('show');
-});
+  btn.addEventListener('click', function(evt){
+    evt.preventDefault;
+    btn.classList.toggle('close');
+    burgerMenu.classList.toggle('show');
+  });
 
-// close.addEventListener('click', function(evt){
-//   evt.preventDefault;
-//   burgerMenu.remove('show');
-// });
+  
+  // if (burgerMenu.classList.toggle('show')) {
+
+  // }
+
+  // close.addEventListener('click', function(evt){
+  //   evt.preventDefault;
+  //   burgerMenu.remove('show');
+  // });
 }
