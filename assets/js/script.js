e4b39052e6b5da1e8c82e82fde23086afdfda5bf
arthur190901 <khachatryan184@icloud.com>
2022-03-18 17:36:09 +0300

    init stylelint

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,34 +11,48 @@
     burgerMenu.classList.toggle('show');
   });
 
-  // btnBefore.addEventListener('click', function(event) {
-  //   event.preventDefault;
-  //   image.classList.add('cat-before');
-  //   image.classList.add('none');
-  // });
+    ymaps.ready(init);
 
-
-  // const imageMap = document.querySelector('.yandex-map');
-
-  ymaps.ready(init);
-  function init(){
-    let myMap = new ymaps.Map('map', {
-      center: [47.25834810, 39.69285439],
-      zoom: 17,
-      controls: ['zoomControl'],
-      behaviors: ['drag'],
-    });
-
-    let placemark = new ymaps.Placemark([47.25834810, 39.69285439], {
-      hintContent: (image),
-    },
-    {
-      iconLayout: 'default#image',
-      iconImageHref: '../cat-energy/assets/img/map-mark.svg',
-      iconImageSize: [106, 106],
-      iconImageOffset: [-58, -106],
-    });
-
-    myMap.geoObjects.add(placemark);
-  }
+    function init () {
+        var myMap = new ymaps.Map("map", {
+            center: [47.25834810, 39.69285439],
+            zoom: 17,
+            controls: ['zoomControl'],
+            behaviors: ['drag'],
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='test'>" +
+            "{{ properties.object }}" +
+            "{{ properties.address }}" +
+            "</div>", {
+                getShape: function () {
+                        var el = this.getElement(),
+                        result = null;
+                    if (el) {
+                        var firstChild = el.firstChild;
+                        result = new ymaps.shape.Rectangle(
+                            new ymaps.geometry.pixel.Rectangle([
+                                [0, 0],
+                                [firstChild.offsetWidth, firstChild.offsetHeight]
+                            ])
+                        );
+                    }
+                    return result;
+                }
+            }
+        );
+        let placemark = new ymaps.Placemark([47.25834810, 39.69285439], {
+            hintLayout: HintLayout,
+            hintContent: 'Its me'
+            },
+            {
+            iconLayout: 'default#image',
+            iconImageHref: '../cat-energy/assets/img/map-mark.svg',
+            iconImageSize: [106, 106],
+            iconImageOffset: [-58, -106],
+            });
+            myMap.geoObjects.add(placemark);
+        myMap.geoObjects.add(myPlacemark);
+    }
 }
