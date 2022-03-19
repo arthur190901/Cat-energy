8cd62004e143d82a5280588549580c8a09eb749e
arthur190901 <khachatryan184@icloud.com>
2022-03-19 16:03:58 +0300

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-// import gulp from 'gulp'
-// import htmlmin from 'gulp-htmlmin'
-// import fileinclude from 'gulp-file-include'
-// import cssmin from 'gulp-cssmin'
-// import image from 'gulp-image'
-// import jsmin from 'gulp-jsmin'
+import gulp from 'gulp'
+import htmlmin from 'gulp-htmlmin'
+import fileinclude from 'gulp-file-include'
+// import scss from 'gulp-scss'
+// import fontmin from 'gulp-fontmin'
+import image from 'gulp-image'
+import jsmin from 'gulp-jsmin'
 // import clean from 'gulp-clean'
 // import browsersync from 'browser-sync'
 
-// function html() {
-//   return gulp
-//     .src(['index.html', 'catalog.html'])
-//     .pipe(
-//       fileinclude({
-//         prefix: '@@',
-//         basepath: '@file',
-//       })
-//     )
-//     .pipe(htmlmin({ collapseWhitespace: true }))
-//     .pipe(gulp.dest('build/'))
-// }
-// gulp.task(html)
+function html() {
+  return gulp
+    .src(['index.html', 'catalog.html'])
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build/'))
+}
+gulp.task(html)
 
-// function css() {
+// function scss() {
 //   return gulp
-//     .src('assets/css/style.css')
-//     .pipe(cssmin())
-//     .pipe(gulp.dest('build/assets/css'))
+//     .src('assets/scss/style.scss')
+//     .pipe(scss())
+//     .pipe(gulp.dest('build/assets/scss'))
 // }
-// gulp.task(css)
+// gulp.task(scss)
 
 // function font() {
-//   return gulp.src('assets/fonts/*.ttf').pipe(gulp.dest('build/assets/fonts'))
+//   return gulp.src('assets/fonts/*.ttf *.woff *.EOT ').pipe(gulp.dest('build/assets/fonts'))
 // }
 // gulp.task(font)
 
@@ -39,32 +40,32 @@
 // }
 // gulp.task(favicon)
 
-// function img() {
-//   return gulp
-//     .src('assets/img/*')
-//     .pipe(
-//       image({
-//         pngquant: true,
-//         optipng: false,
-//         zopflipng: true,
-//         jpegRecompress: false,
-//         mozjpeg: true,
-//         gifsicle: true,
-//         svgo: true,
-//         concurrent: 10,
-//         quiet: true,
-//       })
-//     )
-//     .pipe(gulp.dest('build/assets/img'))
-// }
-// gulp.task(img)
+function img() {
+  return gulp
+    .src('assets/img/*')
+    .pipe(
+      image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: true,
+      })
+    )
+    .pipe(gulp.dest('build/assets/img'))
+}
+gulp.task(img)
 
-// function js() {
-//   return gulp.src(['assets/js/script.js', 'assets/js/login.js'])
-//     .pipe(jsmin())
-//     .pipe(gulp.dest('build/assets/js'))
-// }
-// gulp.task(js)
+function js() {
+  return gulp.src(['assets/js/script.js'])
+    .pipe(jsmin())
+    .pipe(gulp.dest('build/assets/js'))
+}
+gulp.task(js)
 
 // function deleteBuild() {
 //   return gulp.src('build', { read: false, allowEmpty: true }).pipe(clean())
