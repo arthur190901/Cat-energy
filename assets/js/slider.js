bb29a63c8882002e5c137fdde86ff910fcd1c7a2
arthur190901 <khachatryan184@icloud.com>
2022-03-21 16:42:10 +0300

    a commit

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,25 +1,25 @@
 {
   const toggle = document.querySelector('.example__taggle');
-  const imageBefore = document.getElementById('image1');
-  const imageAfter = document.getElementById('image2');
+  const imageBefore = document.getElementById('image2');
+  const imageAfter = document.getElementById('image1');
   const before = document.getElementById('before');
   const after = document.getElementById('after');
 
-  toggle.addEventListener('change', function(evt){
-    console.log(evt, toggle.value);
-    imageAfter.style.width = toggle.value+'%';
-    imageBefore.style.width = toggle.value-'%';
-    console.log(imageBefore);
+  toggle.addEventListener('input', function(){
+    imageAfter.style.width = 100 - toggle.value + '%';
+    imageBefore.style.width = toggle.value + '%';
   });
 
-  before.addEventListener('click', function(event){
-    event.preventDefault;
-    toggle.value = '0'
+  before.addEventListener('click', function(){
+    imageBefore.style.width = '100%'
+    imageAfter.style.width = '0'
+    toggle.value = '0';
   })
 
-  after.addEventListener('click', function(event){
-    event.preventDefault;
-    toggle.value = '100'
+  after.addEventListener('click', function(){
+    imageBefore.style.width = '0'
+    imageAfter.style.width = '100%'
+    toggle.value = '100';
   })
 }
 
